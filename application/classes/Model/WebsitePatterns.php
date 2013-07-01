f8322e8ff51d056cc49a145e639d398060fb9b99
pmeshkov <Pasha.Web@zuzex.com>
2013-07-01 17:57:26 +0400

    Campaign edit method

--- application/classes/Model/WebsitePatterns.php
+++ application/classes/Model/WebsitePatterns.php
@@ -4,5 +4,11 @@ defined('SYSPATH') or die('No direct script access.');
 
 class Model_WebsitePatterns extends ORM
 {
-    protected $_table_name  = 'campaigns_website_patterns';
+    protected $_table_name = 'campaigns_website_patterns';
+    protected $_belongs_to = array(
+		'campaign' => array(
+            'model'       => 'Gampaign',
+            'foreign_key' => 'id_country'
+        )
+	);
 }
--- application/views/scripts/admin/campaigns_add.php
+++ application/views/scripts/admin/campaigns_add.php
@@ -7,18 +7,18 @@
                 <tr>
                     <td><label for="c_name">Campaign name: </label> </td>
                     <td>
-                        <input type="text" name="c_name" id="c_name"  >
+                        <input type="text" name="c_name" id="c_name" value="<?php echo $campaign->name; ?>">
                     </td>
                 </tr>
                 <tr>
                     <td><label for="limit">Clicks limit per 24hrs.: </label> </td>
                     <td>
-                        <input type="text" name="limit" id="limit"  >
+                        <input type="text" name="limit" id="limit" value="<?php echo $campaign->click_limit; ?>">
                     </td>
                 </tr>
             </tbody>
         </table>
-        
+
     </div>
 </div>
 <hr />
@@ -26,7 +26,14 @@
     <div class="span6">
         Websites:
         <div class="sites-container">
-            <div><input type="text" name="sites[]"> <a class="btn btn-danger"><i class="icon-minus icon-white"></i></a></div>
+            <?php foreach ($campaign->patterns->find_all() as $pattern): ?>
+            <div>
+                <input type="text" name="sites[]" value="<?php echo $pattern->pattern; ?>">
+                <a class="btn btn-danger">
+                    <i class="icon-minus icon-white"></i>
+                </a>
+            </div>
+            <?php endforeach; ?>
         </div>
         <a href="" class="btn btn-primary"><i class="icon-plus icon-white"></i> Add </a>
         <a href="" class="btn btn-primary"><i class="icon-upload icon-white"></i> Import from CSV </a>
@@ -34,7 +41,14 @@
     <div class="span6">
         Ad urls:
         <div class="urls-container">
-            <div><input type="text" name="urls[]"> <a class="btn btn-danger"><i class="icon-minus icon-white"></i></a></div>
+            <?php foreach ($campaign->ad_urls->order_by('position', 'asc')->find_all() as $adUrl): ?>
+            <div>
+                <input type="text" name="urls[]" value="<?php echo $adUrl->target_url; ?>">
+                <a class="btn btn-danger">
+                    <i class="icon-minus icon-white"></i>
+                </a>
+            </div>
+            <?php endforeach; ?>
         </div>
         <a href="" class="btn btn-primary"><i class="icon-plus icon-white"></i> Add </a>
         <a href="" class="btn btn-primary"><i class="icon-upload icon-white"></i> Import from CSV </a>
@@ -50,6 +64,6 @@
 </form>
 <script>
     jQuery(document).ready(function(){
-        
+
     });
 </script>
