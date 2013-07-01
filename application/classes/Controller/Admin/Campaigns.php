4a15386ca3c97d67b2b1caed20c362d62ae9d571
Eugeniy Bondar <eugeneb.web@zuzex.com>
2013-07-01 15:06:09 +0400

    ACE-21

--- application/classes/Controller/Admin/Campaigns.php
+++ application/classes/Controller/Admin/Campaigns.php
@@ -6,44 +6,63 @@ class Controller_Admin_Campaigns extends Controller_Auth {
 
     public function action_index()
 	{
+        
+        if (!empty($_POST['delete'])){
+            $this->_delete(array_keys($_POST['delete']));
+        }
         $view = View::factory('scripts/admin/campaigns');
-        
-        $view->campaigns = ORM::factory('Campaigns')->with('countries')->find_all();
-        
-        
-        
-        
-        
-        /*$params = array();
-        
         $page = intval(Arr::get($_GET, 'page', 1));
         $view->page = $page;
         $per_page = 10;
-        $date = Arr::get($_GET, 'date', '');
-        if ($date AND ($date_object = DateTime::createFromFormat('m/d/Y', $date)))
+        $filter = intval(Arr::get($_GET, 'filter', 0));
+        $campaigns = ORM::factory('Campaigns');
+        if ($filter > 0)
         {
-            $date = $date_object->format('m/d/Y');
-            $params['date'] = $date_object->format('Y-m-d');
+            $campaigns->where('id_country', '=', $filter);
         }
-        
-        $view->date = $date;
-        
-        $view->stats = ORM::factory('Stats')->find_by_params($params, $page, $per_page);
-        //echo ORM::factory('Stats')->last_query();
-        
-        $pagination_view = new View('pagination/stats');
+        if ($page < 1)
+        {
+            $page = 1;
+        }
+        if ($per_page)
+        {
+            $campaigns->limit($per_page)->offset(($page-1)*$per_page);
+        }
+        $pagination_view = new View('pagination/campaigns');
         $pagination_view->page = $page;
         $pagination_view->perpage = $per_page;
-        $pagination_view->date = $date;
-        $pagination_view->count_all = ORM::factory('Stats')->count_by_params($params);
-        $view->pagination = $pagination_view->render();*/
-                        
+        $pagination_view->filter = $filter;
+        $pagination_view->count_all = ORM::factory('Campaigns')->count_by_params($filter);
+        $view->pagination = $pagination_view->render();
+        $view->filter = $filter;
+        $view->campaigns = $campaigns->with('countries')->find_all();;//ORM::factory('Campaigns')->with('countries')->find_all();
+        $view->countries = ORM::factory('Countries')->find_all();        
 		$this->display($view);
 	}
     
     public function action_add()
     {
         
+    }
+    
+    protected function _delete($idCampaign = null)
+    {
+        if (!empty($idCampaign))
+        {
+            $campaignsToDelete = ORM::factory('Campaigns');
+            
+            if ( is_array($idCampaign) )
+            {
+                $campaignsToDelete->where('id_campaign', 'in', $idCampaign);
+            }
+            else
+            {
+                $campaignsToDelete->where('id_campaign', '=', $idCampaign);
+            }
+            $campaignsToDelete->find()->delete();
+
+            //TODO: delete related patterns and ad_urls
+        }
     }
     
 }
--- application/classes/Model/Campaigns.php
+++ application/classes/Model/Campaigns.php
@@ -5,9 +5,22 @@ defined('SYSPATH') or die('No direct script access.');
 class Model_Campaigns extends ORM {
 
     protected $_table_name = 'campaigns';
-    protected $_primary_key = 'id';
+    protected $_primary_key = 'id_campaign';
     
     protected $_belongs_to = array(
 		'country' => array('model' => 'Countries', 'foreign_key' => 'id_country'),
 	);
+    
+    public function count_by_params($filter)
+    {
+        if ($filter)
+        {
+            $this->where('id_country', '=', $filter);
+        }
+                
+        return $this->with('countries')->count_all();
+    }
+    
+    
+    
 }
