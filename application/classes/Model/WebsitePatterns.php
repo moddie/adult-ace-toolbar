5591f4862e1710cbb9c12ddc33bf8ef5de1cae4f
pmeshkov <Pasha.Web@zuzex.com>
2013-07-02 19:24:35 +0400

    validation for patterns

--- application/classes/Model/WebsitePatterns.php
+++ application/classes/Model/WebsitePatterns.php
@@ -17,15 +17,37 @@ class Model_WebsitePatterns extends ORM
 	{
 		return array(
 			'pattern' => array(
-				array('not_empty'), array('url'), array('max_length', array(':value', 2000)),
-                /*array(function($value, Validation $object)
-                    {
-                        $object->error('some_field', 'some_error');
-                    }, array(':value', ':validation')),*/
+				array('not_empty'),
+                array('max_length', array(':value', 2000)),
+                array(array($this, 'isValidPattern'), array(':validation', ':field')),
             ),
             'id_campaign' => array(
                 array('not_empty')
             )
         );
     }
+
+    function isValidPattern(Validation $validation, $field)
+    {
+        if (!preg_match('~^\*?[^\*]+\*?$~si', $validation[$field]))
+        {
+            $validation->error($field, 'pattern_not_valid', array($validation[$field]));
+        }
+
+        $checkPatternSql = 'SELECT *
+            FROM `' . $this->_table_name . '` AS `cwp`
+            WHERE
+                :pattern LIKE REPLACE( `cwp`.`pattern` , \'*\', \'%\' )
+                OR `cwp`.`pattern` LIKE REPLACE( :pattern, \'*\', \'%\' )';
+
+        $checkPatternQuery = DB::query(Database::SELECT, $checkPatternSql)
+            ->param(':pattern', $validation[$field]);
+
+        if ($checkPatternQuery->execute()->count() > 0)
+        {
+            $validation->error($field, 'crossing_pattern_exist', array($validation[$field]));
+        }
+
+        return TRUE;
+    } // end isValidPattern
 }
---
+++ application/messages/patterns.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+           'pattern_not_valid' => 'Invalid pattern ":field"',
+           'crossing_pattern_exist' => 'Crossing pattern already exist',
+       );
+?>
