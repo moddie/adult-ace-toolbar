bb4e62128d68415ebaaaddffb7c5b2506951de3c
Alexander Morgun <amorgun@zuzex.com>
2015-02-06 11:26:49 +0300

    Modules

---
+++ application/views/scripts/admin/login_api.php
@@ -0,0 +1,9 @@
+<div style="padding:40px">
+    <form action="/api/user/login" method="post">
+        <table>            
+            <tr><td>Email:</td><td><input type="text" name="email" value="" /></td></tr>
+            <tr><td>Password:</td><td><input type="text" name="password" value="" /></td></tr>
+        </table>    
+        <input type="submit" />    
+    </form>
+<div>
---
+++ application/views/scripts/admin/user_add.php
@@ -0,0 +1,10 @@
+<div style="padding:40px">
+    <form action="/api/user/register" method="post">
+        <table>
+            <tr><td>Username:</td><td> <input type="text" name="username" value="" /></td></tr>
+            <tr><td>Password:</td><td><input type="text" name="password" value="" /></td></tr>
+            <tr><td>Email:</td><td><input type="text" name="email" value="" /></td></tr>
+        </table>    
+        <input type="submit" />    
+    </form>
+<div>
